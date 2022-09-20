4ff1b36f2f441e3f142cce541b1bc79a855d9729
j3rem1e <jeremie.brebec@gmail.com>
2022-09-20 10:41:59 +0200

    Upgrade to groovy 3.13

--- src/test/resources/testgen.groovy
+++ src/test/resources/testgen.groovy
@@ -3,3 +3,9 @@
 def cst() {
     new org.j3rem1e.test.Bridge().files.collect { it.name }
 }
+
+@groovy.transform.CompileStatic
+def cst2() {
+    def f = new org.j3rem1e.test.Bridge().files[0]
+    f.name
+}
